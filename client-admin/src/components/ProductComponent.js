d950e299b0c9dadd5c7d950704536a9589a1c41d
KimAwk <kimanh57682016@gmail.com>
2026-04-02 19:21:45 +0700

    fix submodule issue

---
+++ client-admin/src/components/ProductComponent.js
@@ -0,0 +1,146 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import MyContext from '../contexts/MyContext';
+import ProductDetail from './ProductDetailComponent';
+
+class Product extends Component {
+  static contextType = MyContext; // using this.context to access global state
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      noPages: 0,
+      curPage: 1,
+      itemSelected: null
+    };
+  }
+
+  render() {
+    // ✅ FIX: map an toàn dù products bị undefined
+    const prods = (this.state.products || []).map((item) => {
+      return (
+        <tr
+          key={item._id}
+          className="datatable"
+          onClick={() => this.trItemClick(item)}
+        >
+          <td>{item._id}</td>
+          <td>{item.name}</td>
+          <td>{item.price}</td>
+          <td>{new Date(item.cdate).toLocaleString()}</td>
+
+          {/* ✅ FIX: tránh lỗi khi category = null */}
+          <td>{item.category ? item.category.name : ''}</td>
+
+          <td>
+            <img
+              src={'data:image/jpg;base64,' + item.image}
+              width="100px"
+              height="100px"
+              alt=""
+            />
+          </td>
+        </tr>
+      );
+    });
+
+    const pagination = Array.from({ length: this.state.noPages || 0 }, (_, index) => {
+      if ((index + 1) === this.state.curPage) {
+        return (<span key={index}><b>{index + 1}</b></span>);
+      } else {
+        return (
+          <span
+            key={index}
+            className="link"
+            onClick={() => this.lnkPageClick(index + 1)}
+          >
+            {index + 1}
+          </span>
+        );
+      }
+    });
+
+    return (
+      <div>
+        <div className="float-left">
+          <h2 className="text-center">PRODUCT LIST</h2>
+          <table className="datatable" border="1">
+            <tbody>
+              <tr className="datatable">
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Creation date</th>
+                <th>Category</th>
+                <th>Image</th>
+              </tr>
+              {prods}
+              <tr>
+                <td colSpan="6">{pagination}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="inline" />
+        <ProductDetail
+          item={this.state.itemSelected}
+          curPage={this.state.curPage}
+          updateProducts={this.updateProducts}
+        />
+        <div className="float-clear" />
+      </div>
+    );
+  }
+
+  // ✅ FIX: updateProducts cũng set mặc định để khỏi undefined
+  updateProducts = (products, noPages) => {
+    this.setState({
+      products: products || [],
+      noPages: noPages || 0
+    });
+  };
+
+  componentDidMount() {
+    this.apiGetProducts(this.state.curPage);
+  }
+
+  // event-handlers
+  lnkPageClick(index) {
+    this.apiGetProducts(index);
+  }
+
+  trItemClick(item) {
+    this.setState({ itemSelected: item });
+  }
+
+  // apis
+  apiGetProducts(page) {
+    const config = { headers: { 'x-access-token': this.context.token } };
+
+    // ✅ FIX: ép page >= 1
+    const safePage = page && page > 0 ? page : 1;
+
+    axios
+      .get('/api/admin/products?page=' + safePage, config)
+      .then((res) => {
+        const result = res.data || {};
+        this.setState({
+          products: result.products || [],
+          noPages: result.noPages || 0,
+          curPage: result.curPage || safePage
+        });
+      })
+      .catch(() => {
+        // ✅ FIX: nếu server lỗi/500 thì không crash UI
+        this.setState({
+          products: [],
+          noPages: 0,
+          curPage: 1
+        });
+      });
+  }
+}
+
+export default Product;
